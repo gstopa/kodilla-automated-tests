16b91460bf0a6806a9bc19f0136f4299a13c33aa
Grzegorz Stopa <Grzegorz.Stopa@aptiv.com>
2022-03-31 16:12:48 +0200

    1.1 palindromes easier comparison

--- palindrome.py
+++ palindrome.py
@@ -33,18 +33,7 @@ def prep_string_data(data_to_prep: str) -> str:
     return data_lower_case
 
 
-def compare(data_to_compare: str) -> bool:
-    begin = 0
-    end = len(data_to_compare)
-    begin_half, reminder = divmod(end, 2)
-    if reminder == 0:
-        end_half = begin_half - 1
-    else:
-        end_half = begin_half
-    return data_to_compare[begin:begin_half] == data_to_compare[end:end_half:-1]
-
-
 def is_palindrome(data: Any) -> bool:
     string_data = translate_data_to_unambiguous_string(data)
     prepared_data = prep_string_data(string_data)
-    return compare(prepared_data)
+    return prepared_data == prepared_data[-1::-1]
