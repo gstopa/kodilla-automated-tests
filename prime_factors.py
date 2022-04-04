c0f673953f53327f9c49239520202f30a5412ca9
Grzegorz Stopa <Grzegorz.Stopa@aptiv.com>
2022-04-04 17:33:09 +0200

    1.3 prime factors [REFACTOR] add excludes to coverage as there is unreachable code

--- prime_factors.py
+++ prime_factors.py
@@ -4,7 +4,7 @@ from typing import List
 
 def possible_factors():
     yield 2
-    for value in count(3, step=2):
+    for value in count(3, step=2):  # pragma: no branch
         yield value
 
 
@@ -13,7 +13,7 @@ def prime_factors(number: int) -> List[int]:
         raise ValueError(f"Expected integer, got {type(number)}!")
     factors_to_check = possible_factors()
     found_factors = []
-    for factor in factors_to_check:
+    for factor in factors_to_check:  # pragma: no branch
         while number % factor == 0:
             found_factors.append(factor)
             number //= factor
