9fd6e2b3f60deb05d670510801255e6a0c0f2f34
Grzegorz Stopa <Grzegorz.Stopa@aptiv.com>
2022-04-04 12:49:49 +0200

    1.3 prime factors [RED] add checking ValueError message content

--- test_prime_factors.py
+++ test_prime_factors.py
@@ -9,5 +9,8 @@ def test_prime_factors_returns_list() -> None:
 
 
 def test_prime_factors_raises_valueerror_when_number_is_not_an_integer() -> None:
-    with pytest.raises(ValueError):
+    expectation = "Expected integer, got <class 'float'>!"
+    with pytest.raises(ValueError) as value_error:
         prime_factors(number=2.0)
+    assert str(value_error.value) == expectation, \
+        f"Expected '{expectation}', got '{str(value_error.value)}' for '2.0'!"
