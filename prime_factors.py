4aff06eb9ae0132994f348cfa9228af33a4880b4
Grzegorz Stopa <Grzegorz.Stopa@aptiv.com>
2022-04-04 14:23:18 +0200

    1.3 prime factors [GREEN] make tests pass

--- prime_factors.py
+++ prime_factors.py
@@ -10,6 +10,12 @@ def prime_factors(number: int) -> List[int]:
         number /= 2
     if number % 2 == 0:
         factors.append(2)
+        number /= 2
+    if number % 2 == 0:
+        factors.append(2)
+        number /= 2
+    if number % 2 == 0:
+        factors.append(2)
     if number % 3 == 0:
         factors.append(3)
     if number % 5 == 0:
