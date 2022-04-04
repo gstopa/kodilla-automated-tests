111ecd39a93c1a9831d9c417b648f6096c1f2abf
Grzegorz Stopa <Grzegorz.Stopa@aptiv.com>
2022-04-04 16:23:24 +0200

    1.3 prime factors [GREEN] make tests pass

--- prime_factors.py
+++ prime_factors.py
@@ -1,13 +1,22 @@
+from itertools import count
 from typing import List
+
+
+def possible_factors():
+    yield 2
+    for value in count(3, step=2):
+        yield value
 
 
 def prime_factors(number: int) -> List[int]:
     if not isinstance(number, int):
         raise ValueError(f"Expected integer, got {type(number)}!")
-    factors_to_check = [2, 3, 5, 7, 11, 29]
+    factors_to_check = possible_factors()
     factors = []
     for factor in factors_to_check:
         while number % factor == 0:
             factors.append(factor)
-            number /= factor
+            number //= factor
+        if number == 1:
+            break
     return factors
