78692e8108e63d472d7de8e1de54c446740797ac
Grzegorz Stopa <Grzegorz.Stopa@aptiv.com>
2022-04-04 12:43:49 +0200

    1.3 prime factors [RED] test_prime_factors_raises_valueerror_when_number_is_not_an_integer

--- test_prime_factors.py
+++ test_prime_factors.py
@@ -1,3 +1,4 @@
+import pytest
 from prime_factors import prime_factors
 
 
@@ -5,3 +6,8 @@ def test_prime_factors_returns_list() -> None:
     result = prime_factors(number=2)
     assert isinstance(result, list), \
         f"Expected result to be a list, got {type(result)}!"
+
+
+def test_prime_factors_raises_valueerror_when_number_is_not_an_integer() -> None:
+    with pytest.raises(ValueError):
+        prime_factors(number=2.0)
