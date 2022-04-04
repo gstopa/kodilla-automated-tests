8d80e8f1920711f503596e067443ad52111fa0b0
Grzegorz Stopa <Grzegorz.Stopa@aptiv.com>
2022-04-04 12:56:06 +0200

    1.3 prime factors [GREEN] add checking number is not a string

--- prime_factors.py
+++ prime_factors.py
@@ -4,4 +4,6 @@ from typing import List
 def prime_factors(number: int) -> List[int]:
     if isinstance(number, float):
         raise ValueError("Expected integer, got <class 'float'>!")
+    if isinstance(number, str):
+        raise ValueError("Expected integer, got <class 'str'>!")
     return []
