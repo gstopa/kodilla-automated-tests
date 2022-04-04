20c65f2e4779cc0e8bffc472ad6eb6a51a6819d8
Grzegorz Stopa <Grzegorz.Stopa@aptiv.com>
2022-04-04 13:19:52 +0200

    1.3 prime factors [GREEN] make tests green

--- prime_factors.py
+++ prime_factors.py
@@ -4,4 +4,4 @@ from typing import List
 def prime_factors(number: int) -> List[int]:
     if not isinstance(number, int):
         raise ValueError(f"Expected integer, got {type(number)}!")
-    return [2]
+    return [number]
