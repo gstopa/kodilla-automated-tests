aea4e376d2a78b253f43e837cf1114cd550a37fc
Grzegorz Stopa <Grzegorz.Stopa@aptiv.com>
2022-04-04 12:29:44 +0200

    1.3 prime factors [REFACTOR] rename test to be more precise

--- test_prime_factors.py
+++ test_prime_factors.py
@@ -1,6 +1,6 @@
 from prime_factors import prime_factors
 
 
-def test_prime_factors() -> None:
+def test_prime_factors_returns_list() -> None:
     result = prime_factors(number=0)
     assert isinstance(result, list)
