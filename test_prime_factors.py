9edafafa72cb5d58f42c15155f371307a4ec178f
Grzegorz Stopa <Grzegorz.Stopa@aptiv.com>
2022-04-04 12:59:36 +0200

    1.3 prime factors [REFACTOR] use parametrize for ValueErrors

--- test_prime_factors.py
+++ test_prime_factors.py
@@ -1,3 +1,4 @@
+from typing import Any
 import pytest
 from prime_factors import prime_factors
 
@@ -8,14 +9,15 @@ def test_prime_factors_returns_list() -> None:
         f"Expected result to be a list, got {type(result)}!"
 
 
-def test_prime_factors_raises_valueerror_when_number_is_not_an_integer() -> None:
-    expectation = "Expected integer, got <class 'float'>!"
+@pytest.mark.parametrize(
+    "data,expectation",
+    [
+        (2.0, "Expected integer, got <class 'float'>!"),
+        ("2.0", "Expected integer, got <class 'str'>!"),
+    ],
+)
+def test_prime_factors_raises_valueerror_when_number_is_not_an_integer(data: Any, expectation: str) -> None:
     with pytest.raises(ValueError) as value_error:
-        prime_factors(number=2.0)
+        prime_factors(number=data)
     assert str(value_error.value) == expectation, \
-        f"Expected '{expectation}', got '{str(value_error.value)}' for '2.0'!"
-    expectation = "Expected integer, got <class 'str'>!"
-    with pytest.raises(ValueError) as value_error:
-        prime_factors(number="2.0")
-    assert str(value_error.value) == expectation, \
-        f"Expected '{expectation}', got '{str(value_error.value)}' for '\"2.0\"'!"
+        f"Expected '{expectation}', got '{str(value_error.value)}' for '{data}'!"
