f65e5b5bc025622154a2eab0b50b63e44e733a45
Grzegorz Stopa <Grzegorz.Stopa@aptiv.com>
2022-04-04 16:20:18 +0200

    1.3 prime factors [GREEN] make tests pass

--- prime_factors.py
+++ prime_factors.py
@@ -4,7 +4,7 @@ from typing import List
 def prime_factors(number: int) -> List[int]:
     if not isinstance(number, int):
         raise ValueError(f"Expected integer, got {type(number)}!")
-    factors_to_check = [2, 3, 5, 7, 11]
+    factors_to_check = [2, 3, 5, 7, 11, 29]
     factors = []
     for factor in factors_to_check:
         while number % factor == 0:
