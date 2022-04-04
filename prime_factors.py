0820a846e20f51b5dbd68941b6f725725d0098b1
Grzegorz Stopa <Grzegorz.Stopa@aptiv.com>
2022-04-04 13:01:50 +0200

    1.3 prime factors [GREEN] make tests pass

--- prime_factors.py
+++ prime_factors.py
@@ -6,4 +6,6 @@ def prime_factors(number: int) -> List[int]:
         raise ValueError("Expected integer, got <class 'float'>!")
     if isinstance(number, str):
         raise ValueError("Expected integer, got <class 'str'>!")
+    if isinstance(number, list):
+        raise ValueError(f"Expected integer, got <class 'list'>!")
     return []
