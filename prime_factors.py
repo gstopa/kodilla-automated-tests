f2c97126328f8b3879be6189fbc9e8838e8afe1e
Grzegorz Stopa <Grzegorz.Stopa@aptiv.com>
2022-04-04 14:26:43 +0200

    1.3 prime factors [REFACTOR] simpler way of creating multiple 2 factors

--- prime_factors.py
+++ prime_factors.py
@@ -5,17 +5,9 @@ def prime_factors(number: int) -> List[int]:
     if not isinstance(number, int):
         raise ValueError(f"Expected integer, got {type(number)}!")
     factors = []
-    if number % 2 == 0:
+    while number % 2 == 0:
         factors.append(2)
         number /= 2
-    if number % 2 == 0:
-        factors.append(2)
-        number /= 2
-    if number % 2 == 0:
-        factors.append(2)
-        number /= 2
-    if number % 2 == 0:
-        factors.append(2)
     if number % 3 == 0:
         factors.append(3)
     if number % 5 == 0:
