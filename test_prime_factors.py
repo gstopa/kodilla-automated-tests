6622415b41ef6129be5abca5171ae3266859eb8f
Grzegorz Stopa <Grzegorz.Stopa@aptiv.com>
2022-04-04 12:52:49 +0200

    1.3 prime factors [RED] add another case for ValueError

--- test_prime_factors.py
+++ test_prime_factors.py
@@ -14,3 +14,8 @@ def test_prime_factors_raises_valueerror_when_number_is_not_an_integer() -> None
         prime_factors(number=2.0)
     assert str(value_error.value) == expectation, \
         f"Expected '{expectation}', got '{str(value_error.value)}' for '2.0'!"
+    expectation = "Expected integer, got <class 'str'>!"
+    with pytest.raises(ValueError) as value_error:
+        prime_factors(number="2.0")
+    assert str(value_error.value) == expectation, \
+        f"Expected '{expectation}', got '{str(value_error.value)}' for '\"2.0\"'!"
