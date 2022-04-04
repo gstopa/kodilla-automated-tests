1d22e12c4ef6da50f741f79e9da86802457abb8b
Grzegorz Stopa <Grzegorz.Stopa@aptiv.com>
2022-04-04 12:41:25 +0200

    1.3 prime factors [REFACTOR] more precise assert error description

--- test_prime_factors.py
+++ test_prime_factors.py
@@ -3,4 +3,5 @@ from prime_factors import prime_factors
 
 def test_prime_factors_returns_list() -> None:
     result = prime_factors(number=2)
-    assert isinstance(result, list)
+    assert isinstance(result, list), \
+        f"Expected result to be a list, got {type(result)}!"
