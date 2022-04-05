830e00ca3d208c6820362f52d362b2d44599fb42
Grzegorz Stopa <Grzegorz.Stopa@aptiv.com>
2022-04-05 22:14:12 +0200

    2.2 romans to_romans raises type error when number is not an integer

--- src/romans/romans.py
+++ src/romans/romans.py
@@ -1,2 +1,4 @@
-def to_romans(number):
+def to_romans(number: int) -> str:
+    if not isinstance(number, int):
+        raise TypeError
     return "I"
--- tests/romans/test_romans.py
+++ tests/romans/test_romans.py
@@ -1,5 +1,15 @@
+from typing import Any
+
+import pytest
+
 from romans.romans import to_romans
 
 
 def test_to_romans_convert_1_to_i() -> None:
     assert to_romans(number=1) == "I"
+
+
+@pytest.mark.parametrize("data", [1.0, "1", []])
+def test_to_romans_raises_typeerror_when_number_is_not_integer(data: Any) -> None:
+    with pytest.raises(TypeError):
+        to_romans(number=data)
