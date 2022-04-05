e167b97ae30ca4dd09c51719ccdbf39908f224b0
Grzegorz Stopa <Grzegorz.Stopa@aptiv.com>
2022-04-05 22:35:28 +0200

    2.2 romans to_romans raises ValueError when number is not in range from 1 to 3999

--- src/romans/romans.py
+++ src/romans/romans.py
@@ -1,4 +1,6 @@
 def to_romans(number: int) -> str:
     if not isinstance(number, int):
         raise TypeError(f"Expected number to be an integer, got {type(number)}!")
+    if number not in range(1, 4000):
+        raise ValueError(f"Expected number to be in range [1; 3999], got {number}!")
     return "I"
--- tests/romans/test_romans.py
+++ tests/romans/test_romans.py
@@ -13,3 +13,9 @@ def test_to_romans_convert_1_to_i() -> None:
 def test_to_romans_raises_typeerror_when_number_is_not_integer(data: Any) -> None:
     with pytest.raises(TypeError):
         to_romans(number=data)
+
+
+@pytest.mark.parametrize("data", [-1, 0, 4000, 4001])
+def test_to_romans_raises_valueerror_when_number_not_in_range_1_to_3999_both_inclusive(data: int) -> None:
+    with pytest.raises(ValueError):
+        to_romans(number=data)
