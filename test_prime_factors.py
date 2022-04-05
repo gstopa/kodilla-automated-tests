973d3d9898aa5d904d436533a014277f5bc2cd7c
Grzegorz Stopa <Grzegorz.Stopa@aptiv.com>
2022-04-05 11:43:00 +0200

    1.3 prime factors [REFACTOR] remove not needed test_prime_factors_returns_list

--- test_prime_factors.py
+++ test_prime_factors.py
@@ -3,12 +3,6 @@ import pytest
 from prime_factors import prime_factors
 
 
-def test_prime_factors_returns_list() -> None:
-    result = prime_factors(number=2)
-    assert isinstance(result, list), \
-        f"Expected result to be a list, got {type(result)}!"
-
-
 @pytest.mark.parametrize(
     "data,expectation",
     [
