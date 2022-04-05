f5d9226af9884956c020341f74bdaa1716464071
Grzegorz Stopa <Grzegorz.Stopa@aptiv.com>
2022-04-05 11:45:07 +0200

    1.3 prime factors [REFACTOR] simplify test_prime_factors_raises_valueerror_when_number_is_not_an_integer

--- test_prime_factors.py
+++ test_prime_factors.py
@@ -3,19 +3,10 @@ import pytest
 from prime_factors import prime_factors
 
 
-@pytest.mark.parametrize(
-    "data,expectation",
-    [
-        (2.0, "Expected integer, got <class 'float'>!"),
-        ("2.0", "Expected integer, got <class 'str'>!"),
-        ([], "Expected integer, got <class 'list'>!"),
-    ],
-)
-def test_prime_factors_raises_valueerror_when_number_is_not_an_integer(data: Any, expectation: str) -> None:
-    with pytest.raises(ValueError) as value_error:
+@pytest.mark.parametrize("data", [2.0, "2.0", []])
+def test_prime_factors_raises_valueerror_when_number_is_not_an_integer(data: Any) -> None:
+    with pytest.raises(ValueError):
         prime_factors(number=data)
-    assert str(value_error.value) == expectation, \
-        f"Expected '{expectation}', got '{str(value_error.value)}' for '{data}'!"
 
 
 @pytest.mark.parametrize("data", [2, 11])
