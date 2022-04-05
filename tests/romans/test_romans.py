34961c682e1d4894cc625d7926126bb0bf3b8694
Grzegorz Stopa <Grzegorz.Stopa@aptiv.com>
2022-04-05 22:38:39 +0200

    2.2 romans to_romans change tests sequence

--- tests/romans/test_romans.py
+++ tests/romans/test_romans.py
@@ -5,10 +5,6 @@ import pytest
 from romans.romans import to_romans
 
 
-def test_to_romans_convert_1_to_i() -> None:
-    assert to_romans(number=1) == "I"
-
-
 @pytest.mark.parametrize("data", [1.0, "1", []])
 def test_to_romans_raises_typeerror_when_number_is_not_integer(data: Any) -> None:
     with pytest.raises(TypeError):
@@ -19,3 +15,7 @@ def test_to_romans_raises_typeerror_when_number_is_not_integer(data: Any) -> Non
 def test_to_romans_raises_valueerror_when_number_not_in_range_1_to_3999_both_inclusive(data: int) -> None:
     with pytest.raises(ValueError):
         to_romans(number=data)
+
+
+def test_to_romans_convert_1_to_i() -> None:
+    assert to_romans(number=1) == "I"
