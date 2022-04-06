9bab24d92c3f22eaccb30024e82b2c5c2697a3a3
Grzegorz Stopa <Grzegorz.Stopa@aptiv.com>
2022-04-06 11:01:23 +0200

    2.2 romans rename tests

--- tests/romans/test_romans.py
+++ tests/romans/test_romans.py
@@ -29,7 +29,7 @@ def test_to_romans_raises_valueerror_when_number_not_in_range_1_to_3999_both_inc
         (1000, "M"),
     ],
 )
-def test_to_romans_convert_number_into_single_character_roman_number(data: int, expectation: str) -> None:
+def test_to_romans_convert_number_into_single_roman_character(data: int, expectation: str) -> None:
     assert to_romans(number=data) == expectation
 
 
@@ -44,5 +44,5 @@ def test_to_romans_convert_number_into_single_character_roman_number(data: int, 
         (900, "CM"),
     ],
 )
-def test_to_romans_convert_number_into_complex_two_characters_roman_number(data: int, expectation: str) -> None:
+def test_to_romans_convert_number_into_complex_one_less_than_roman_characters(data: int, expectation: str) -> None:
     assert to_romans(number=data) == expectation
