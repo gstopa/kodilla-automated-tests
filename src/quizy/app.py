ba5fd764feece9455b736cc2428e3e0d0bb35a6b
Grzegorz Stopa <Grzegorz.Stopa@aptiv.com>
2022-04-12 18:21:50 +0200

    2.4 quizy Add scoring and all good stuff

--- src/quizy/app.py
+++ src/quizy/app.py
@@ -1,20 +1,74 @@
-from flask import Flask, redirect, render_template, url_for
+from dataclasses import dataclass
+from typing import Dict, Any, List
+from uuid import uuid4
+from flask import Flask, redirect, render_template, request, session, url_for
 from flask_sqlalchemy import SQLAlchemy
-from flask_user import login_required, UserManager, UserMixin
+from flask_user import login_required, UserManager, UserMixin, current_user
+from requests import get
 
 
-RANKING = [
-    {"quiz_uuid": "1234-abcdef-123456-abcd", "user_id": 1, "score": 6},
-    {"quiz_uuid": "1234-abcdef-123456-abce", "user_id": 2, "score": 5},
-    {"quiz_uuid": "1234-abcdef-123456-abcf", "user_id": 3, "score": 8},
-]
+POINTS_MULTIPLIER: Dict[str, int] = {
+    'easy': 1,
+    'medium': 3,
+    'hard': 6,
+}
+
+@dataclass
+class QuizQuestion:
+    question: str
+    correct_answer: str
+
+
+@dataclass
+class QuizTest:
+    uuid: str
+    difficulty: str
+    questions: Dict[str, QuizQuestion]
+
+
+RANKING: List[Dict[str, Any]] = []
+QUIZZES: Dict[str, QuizTest] = {}
 
 
 def get_ranking(sort: bool = True, reverse: bool = True) -> list:
     ranking = RANKING
     if sort:
-        ranking = sorted(RANKING, key=lambda result: result["score"], reverse=reverse)
+        ranking = sorted(RANKING, key=lambda result: result['score'], reverse=reverse)
     return ranking
+
+
+def calculate_quiz_score(quiz_uuid, answers):
+    quiz = QUIZZES[quiz_uuid]
+    multiplier = POINTS_MULTIPLIER[quiz.difficulty]
+    questions = quiz.questions
+    questions_points = [
+        multiplier
+        for key in questions
+        if questions[key].correct_answer == answers[key]
+    ]
+    return sum(questions_points)
+
+
+def add_score_to_ranking(quiz_uuid, user_id, score):
+    RANKING.append(
+        {
+            'quiz_uuid': quiz_uuid,
+            'user_id': user_id,
+            'score': score,
+        }
+    )
+
+
+def create_new_quiz(difficulty) -> str:
+    quiz_uuid = str(uuid4())
+    response = get(f'https://opentdb.com/api.php?amount=10&difficulty={difficulty}&type=boolean')
+    questions = response.json()['results']
+    quiz_questions = {
+        str(index): QuizQuestion(question=question['question'], correct_answer=question['correct_answer'])
+        for index, question in enumerate(questions)
+    }
+    QUIZZES[quiz_uuid] = QuizTest(uuid=quiz_uuid, difficulty=difficulty, questions=quiz_questions)
+    return quiz_uuid
 
 
 class ConfigClass(object):
@@ -24,7 +78,7 @@ class ConfigClass(object):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///'  # in memory SQL database
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # Avoids SQLAlchemy warning
     # Flask-User settings
-    USER_APP_NAME = "quizy"  # Shown in and email templates and page footers
+    USER_APP_NAME = 'quizy'  # Shown in and email templates and page footers
     USER_ENABLE_EMAIL = False  # Disable email authentication
     USER_ENABLE_USERNAME = True  # Enable username authentication
     USER_REQUIRE_RETYPE_PASSWORD = False  # Simplify register form
@@ -47,29 +101,51 @@ def create_app() -> Flask:
 
     @app.route('/')
     def index_page():
-        return render_template("index.html")
+        return render_template('index.html')
 
     @app.route('/ranking')
     def ranking_page():
-        return render_template("ranking.html", ranking=get_ranking())
+        return render_template('ranking.html', ranking=get_ranking())
 
     @app.route('/ranking.json')
     def ranking_json_page():
-        return {"ranking": get_ranking()}
+        return {'ranking': get_ranking()}
 
     @app.route('/quizy_choose')
-    @login_required
+    # @login_required
     def quizy_choose_page():
-        return render_template("quizy_choose.html")
+        return render_template('quizy_choose.html')
 
-    @app.route('/quizy_take')
-    @login_required
-    def quizy_take_page():
-        return render_template("quizy_take.html")
+    @app.route('/quizy_create', methods=['GET', 'POST'])
+    # @login_required
+    def quizy_create_page():
+        if request.method != 'POST':
+            return redirect(url_for('quizy_choose_page'))
+        difficulty = request.form.get('difficulty')
+        if difficulty not in {'easy', 'medium', 'hard'}:
+            return redirect(url_for('quizy_choose_page'))
+        quiz_uuid = create_new_quiz(difficulty=request.form['difficulty'])
+        return redirect(url_for('quizy_take_page', uuid=quiz_uuid))
 
-    @app.route('/quizy_count_me_in')
-    @login_required
+    @app.route('/quizy_take/<uuid>')
+    # @login_required
+    def quizy_take_page(uuid):
+        if uuid not in QUIZZES:
+            return redirect(url_for('quizy_choose_page'))
+        quiz = QUIZZES[uuid]
+        session['quiz_uuid'] = uuid
+        return render_template('quizy_take.html', quiz=quiz.questions)
+
+    @app.route('/quizy_count_me_in', methods=['GET', 'POST'])
+    # @login_required
     def quizy_count_me_in_page():
-        return redirect(url_for("ranking_page"))
+        if request.method != 'POST':
+            return redirect(url_for('quizy_choose_page'))
+        print(request.form)
+        quiz_uuid = session.pop('quiz_uuid')
+        score = calculate_quiz_score(quiz_uuid=quiz_uuid, answers=request.form)
+        user_id = current_user.id if current_user.is_authenticated else 666
+        add_score_to_ranking(quiz_uuid=quiz_uuid, user_id=user_id, score=score)
+        return render_template('quizy_count_me_in.html', score=score, uuid=quiz_uuid)
 
     return app
