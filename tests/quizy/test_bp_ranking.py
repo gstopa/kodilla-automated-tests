fb1aabfce00d5afa576b7ef10eeba6d1fbec522c
Grzegorz Stopa <Grzegorz.Stopa@aptiv.com>
2022-04-18 15:05:04 +0200

    2.4 quizy Refactor tests of bp_ranking by separating it from underlay data module

--- tests/quizy/test_bp_ranking.py
+++ tests/quizy/test_bp_ranking.py
@@ -1,27 +1,39 @@
-def test_ranking_show_page_is_presented(test_client) -> None:
+from unittest.mock import patch, MagicMock
+
+from flask.testing import FlaskClient
+
+from quizy.data_models import QuizResult
+
+
+@patch('quizy.data.QuizyData.get_ranking')
+def test_ranking_show_page_is_presented(get_ranking_mock: MagicMock, test_client: FlaskClient) -> None:
+    get_ranking_mock.return_value = []
     response = test_client.get('/ranking/show')
     assert response.status_code == 200
     assert "This is the quizzes ranking!" in response.text
 
 
-def test_ranking_json_page_is_presented(test_client) -> None:
+@patch('quizy.data.QuizyData.get_ranking')
+def test_ranking_json_page_is_presented(get_ranking_mock: MagicMock, test_client: FlaskClient) -> None:
+    get_ranking_mock.return_value = []
     response = test_client.get('/ranking/json')
     assert response.status_code == 200
     assert response.is_json
     assert 'ranking' in response.json
 
 
-def test_non_empty_ranking(test_client) -> None:
-    response = test_client.post('/quizy/create', data={'difficulty': 'easy'})
-    uuid = response.location.split('/')[-1]
-    test_client.get(response.location)
-    answers = {
-        str(index): 'True'
-        for index in range(10)
-    }
-    test_client.post('/quizy/count_me_in', data=answers)
+@patch('quizy.data.QuizyData.get_ranking')
+def test_non_empty_ranking(get_ranking_mock: MagicMock, test_client: FlaskClient) -> None:
+    ranking = [
+        QuizResult(uuid='12345678-abcd-effe-dcba-876543210099', user_id=666, score=60),
+        QuizResult(uuid='12345678-abcd-effe-dcba-876543210909', user_id=666, score=30),
+        QuizResult(uuid='12345678-abcd-effe-dcba-876543219009', user_id=666, score=10),
+    ]
+    get_ranking_mock.return_value = ranking
     response = test_client.get('/ranking/json')
     assert response.status_code == 200
     assert response.is_json
     assert 'ranking' in response.json
-    assert response.json['ranking'][0]['uuid'] == uuid
+    assert response.json['ranking'][0]['uuid'] == '12345678-abcd-effe-dcba-876543210099'
+    assert response.json['ranking'][1]['uuid'] == '12345678-abcd-effe-dcba-876543210909'
+    assert response.json['ranking'][2]['uuid'] == '12345678-abcd-effe-dcba-876543219009'
