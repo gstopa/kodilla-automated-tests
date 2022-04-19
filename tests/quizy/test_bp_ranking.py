12395e15cae631474c7933da744cf2472f67df06
Grzegorz Stopa <Grzegorz.Stopa@aptiv.com>
2022-04-19 13:54:59 +0200

    2.4 quizy Refactor bp_ranking tests

--- tests/quizy/test_bp_ranking.py
+++ tests/quizy/test_bp_ranking.py
@@ -1,4 +1,4 @@
-from unittest.mock import patch, MagicMock
+from typing import List
 
 import pytest
 from flask import Flask
@@ -8,32 +8,32 @@ from quizy.data import QuizyData
 from quizy.data_models import QuizResult
 
 
-@pytest.fixture(name='test_client_with_ranking')
-def fixture_test_client_with_ranking(app: Flask) -> FlaskClient:
+@pytest.fixture(name='ranking')
+def fixture_ranking(app: Flask) -> List[QuizResult]:
     quizzes_taken = [
-        QuizResult(uuid='12345678-abcd-effe-dcba-876543219009', user_id=666, score=10),
         QuizResult(uuid='12345678-abcd-effe-dcba-876543210099', user_id=666, score=60),
         QuizResult(uuid='12345678-abcd-effe-dcba-876543210909', user_id=666, score=30),
+        QuizResult(uuid='12345678-abcd-effe-dcba-876543219009', user_id=666, score=10),
     ]
     app.config['QuizyData'] = QuizyData(quizzes={}, quizzes_taken=quizzes_taken)
-    return app.test_client()
+    return quizzes_taken
 
 
-def test_ranking_show_page_is_presented(test_client_with_ranking: FlaskClient) -> None:
-    response = test_client_with_ranking.get('/ranking/show')
+def test_ranking_show_page_is_presented(test_client: FlaskClient, ranking: List[QuizResult]) -> None:
+    response = test_client.get('/ranking/show')
     assert response.status_code == 200
     assert 'This is the quizzes ranking!' in response.text
-    assert '12345678-abcd-effe-dcba-876543219009' in response.text
-    assert '12345678-abcd-effe-dcba-876543210099' in response.text
-    assert '12345678-abcd-effe-dcba-876543210909' in response.text
+    assert ranking[0].uuid in response.text
+    assert ranking[1].uuid in response.text
+    assert ranking[2].uuid in response.text
 
 
-def test_non_empty_ranking(test_client_with_ranking: FlaskClient) -> None:
-    response = test_client_with_ranking.get('/ranking/json')
+def test_non_empty_ranking(test_client: FlaskClient, ranking: List[QuizResult]) -> None:
+    response = test_client.get('/ranking/json')
     assert response.status_code == 200
     assert response.is_json
     assert 'ranking' in response.json
     assert len(response.json['ranking']) == 3
-    assert response.json['ranking'][0]['uuid'] == '12345678-abcd-effe-dcba-876543210099'
-    assert response.json['ranking'][1]['uuid'] == '12345678-abcd-effe-dcba-876543210909'
-    assert response.json['ranking'][2]['uuid'] == '12345678-abcd-effe-dcba-876543219009'
+    assert response.json['ranking'][0]['uuid'] == ranking[0].uuid
+    assert response.json['ranking'][1]['uuid'] == ranking[1].uuid
+    assert response.json['ranking'][2]['uuid'] == ranking[2].uuid
